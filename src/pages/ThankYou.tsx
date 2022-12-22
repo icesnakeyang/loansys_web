5e0a7bae2ed49a1b83750a9afdb09f176598a97e
liuyang <flow_ocean@yahoo.com>
2022-12-22 17:10:35 +0800

    progress

--- src/pages/ThankYou.tsx
+++ src/pages/ThankYou.tsx
@@ -1,6 +1,6 @@
 import {CopyrightOutlined, MenuOutlined} from "@ant-design/icons";
 import {useState} from "react";
-import {Menu, Modal} from "antd";
+import {Breadcrumb, Menu, Modal} from "antd";
 import {useTranslation} from "react-i18next";
 
 const ThankYou = () => {
@@ -8,6 +8,9 @@ const ThankYou = () => {
     const {t} = useTranslation()
     return (
         <div style={{height: '100vh'}}>
+            <Breadcrumb style={{margin: 20}}>
+                <Breadcrumb.Item><a href='/'>Home</a> </Breadcrumb.Item>
+            </Breadcrumb>
             <div style={{padding: 40}}>
                 <div style={{fontSize: 30, fontWeight: '350', marginTop: 80}}>{t('thankYou.thankYou')}</div>
                 <div style={{marginTop: 30}}>
